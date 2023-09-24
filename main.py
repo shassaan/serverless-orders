e7ea2860f2dc4d4dea95f86af2fa9e5d0634670a
shassaan.ahmed <hassaan.ahmed@sadapay.pk>
2023-09-24 23:59:53 +0500

    fix: lambda handler

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import importlib
 def handler(event, context):
     try:
         service_name = os.getenv('SERVICE',None)
-        app = importlib.import_module(f'src.services.{service_name}.app')
+        app = importlib.import_module(f'services.{service_name}.app')
         app.run(event,context)
         if service_name is None:
             raise Exception("no service name specified!")
     except Exception as ex:
-        exec.log.critical(f"critical exception occured {ex}")
+        print(f"critical exception occured {ex}")
         raise
